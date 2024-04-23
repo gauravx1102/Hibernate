4553d15ba99607d09b0574efeb2ec685237bdfd5
fulara1102 <fularagaurav1102@gmail.com>
2024-04-23 10:54:36 +0530

    Added : App class

--- hibernate/src/main/java/com/fulara/App.java
+++ hibernate/src/main/java/com/fulara/App.java
@@ -1,7 +1,12 @@
 package com.fulara;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Date;
 
 /**
  * Hello world!
@@ -9,7 +14,7 @@ import org.hibernate.cfg.Configuration;
  */
 public class App
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
         System.out.println( "Hello World!" );
 
@@ -18,19 +23,37 @@ public class App
 
         SessionFactory factory = cfg.buildSessionFactory();
 
-        //creating student
+        //creating Student
 
-        student st = new student();
-        st.setId(100);
-        st.setCity("Chakhutiya");
+        Student st = new Student();
+        st.setId(101);
+        st.setCity("Dwarahat");
         st.setName("Gaurav");
+        st.setEmail("fularagaurav1102@gmail.com");
         System.out.println(st);
+
+
+        //
+        Address add = new Address();
+        add.setStreet("Chakhutiya");
+        add.setCity("Almora");
+        add.setOpen(true);
+        add.setAddedDate(new Date());
+        add.setX(123.123);
+
+        //Reading Image
+
+        FileInputStream fis = new FileInputStream("src/main/java/pic.jpg");
+        byte[] data = new byte[fis.available()];
+        fis.read(data);
+        add.setImage(data);
 
         Session session = factory.openSession();
 
-        session.beginTransaction();
+        Transaction tx = session.beginTransaction();
 
         session.save(st);
+        session.save(add);
 
         session.getTransaction().commit();
 
