e978f2fb7b9091cc10d5a5080078e274b1e26ca2
fulara1102 <fularagaurav1102@gmail.com>
2024-04-23 10:56:20 +0530

    Added : get() and load() method class

--- hibernate/src/main/java/com/fulara/fetchDemo.java
+++ hibernate/src/main/java/com/fulara/fetchDemo.java
@@ -1,2 +1,25 @@
-package com.fulara;public class fetchDemo {
+package com.fulara;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class fetchDemo {
+    public static void main(String[] args) {
+        //get and load method
+        Configuration cfg = new Configuration();
+        cfg.configure();
+        SessionFactory factory = cfg.buildSessionFactory();
+        Session session = factory.openSession();
+
+        Student student = (Student)session.load(Student.class, 101);
+        System.out.println(student);
+
+//        Address ad = (Address)session.get(Address.class,1);
+//        System.out.println(ad);
+//        System.out.println(ad.getCity()+" "+ad.getStreet());
+
+        session.close();
+        factory.close();
+    }
 }
